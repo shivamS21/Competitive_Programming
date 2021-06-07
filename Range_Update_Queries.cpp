4184361af90e51297e7eb5df57bff81959f78e44
shivamsha-iitj <2019UCS0100@iitjammu.ac.in>
2021-06-07 18:23:26 +0530

    7th June

--- Range_Update_Queries.cpp
+++ Range_Update_Queries.cpp
@@ -1,62 +1,68 @@
-// we need to make three functions only to make a code which will be handling the range-query problems.
-// we will be making the tree implicitly
-// first function = building the tree which uses the array of size 4 times the size of the original array
-// second function = to sum the values in the segtreement from l to r
-// third function = to update the values in the implicit array at certain position let say, pos
+//...START BY DOING WHAT IS NECESSARY, THEN WHAT IS POSSIBLE AND SUDDENLY YOU ARE DOING THE IMPOSSIBLE... 
+#include <bits/stdc++.h>
+using namespace std;typedef long long ll;
+#define FAST_FURIER ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define mk make_pair
+#define sorta(v)        sort(v.begin(), v.end())
+#define sortd(v)        sort(v.begin(), v.end(), comp)
+#define rep(i,a,N)      for(ll i=a;i<N;i++)
+#define rrep(i,a,N)     for(ll i=a;i>N;i--)
+#define print(v)        for(ll ite=0;ite<v.size();ite++){cout<<v[ite]<<' ';}cout<<endl;
+#define M 1000000007
  
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-const int N = 2e5+1;
-void build(ll x[], ll segtree[],int vertex, int start, int end){
-    if(start == end) segtree[vertex] = x[start];
-    else{
-        ll mid = (start+end)/2;
-        build(x,segtree,2*vertex,start,mid);
-        build(x,segtree,2*vertex+1,mid+1,end);
-        segtree[vertex] += (segtree[2*vertex]+segtree[2*vertex+1]);
-    }
+bool comp(ll x,ll y){
+    return x > y;
 }
-ll rangesum(ll segtree[], int vertex, int start, int end, int l, int r){
-    if(l>r) return 0;
-    if(start == l and end == r) return segtree[vertex];
+ 
+/*...............code starts here................*/
+// C is first won in M
+void update(ll seg[], int index, int start, int end, int post, ll value){
+    if(start == end){
+        seg[index] += value;
+        return;
+    }
+    int mid = (start + end)/2;
+    if(post <= mid)
+    update(seg, 2*index, start, mid, post, value);
+    else update(seg, 2*index+1, mid+1, end, post, value);
+    seg[index] = seg[2*index]+seg[2*index+1];
+} 
+ll rangesum(ll seg[], int index, int start, int end, int l, int r){
+    if(l > r) return 0;
+    if(start == l and end == r)
+    return seg[index];
     int mid = (start+end)/2;
-    ll sum1 = rangesum(segtree, 2*vertex, start, mid, l, min(r, mid));
-    ll sum2 = rangesum(segtree, 2*vertex+1, mid+1, end, max(mid+1, l), r);
-    return sum1+sum2; 
+    ll sum1 = rangesum(seg, 2*index, start, mid, l, min(r, mid));
+    ll sum2 = rangesum(seg, 2*index+1, mid+1, end, max(mid+1, l), r);
+    return sum1 + sum2;
 }
-void update(ll segtree[], int vertex, int start, int end, int pos, ll val){
-    //update for sum of elements vector.
-    if(start == end) segtree[vertex] += val;
-    else{
-        int mid = (start+end)/2;
-        if(pos <= mid) update(segtree, 2*vertex, start, mid, pos, val);
-        else update(segtree, 2*vertex+1, mid+1, end, pos, val);
-        segtree[vertex] = segtree[2*vertex] + segtree[2*vertex+1];
-    }
-}
-int main(){
-    int n,q; cin >> n >> q;
+void solve(){
+    ll m,n,k;
+    cin >> n >> k;
     ll arr[n];
-    for(int i=0;i<n;i++) cin >> arr[i];
-    ll segtree[4*N]={0};
-    ll x[n+1]={0};
-    build(x, segtree, 1, 0, n);
-    // for(int i=0;i<21;i++) cout << segtree[i] << " ";
-    //here l and r are entered as per the 0th based indexing
-    for(int i=0;i<q;i++){
-    	int check;
-        if(check==1){
-            //increament
-            int a, b, u; cin >> a >> b >> u;
-            update(segtree, 1, 0, n, a-1, u);
-            update(segtree, 1, 0, n, b, u);
+    rep(i,0,n) cin >> arr[i];
+    ll seg[4*n+10]={0};
+
+    rep(i,0,k){
+        int check; cin >> check;
+        if(check == 1){
+            int a, b; ll u; cin >> a >> b >> u;
+            update(seg, 1, 0, n, a-1, u);
+            update(seg, 1, 0, n, b, -u);
         }
         else{
-            //tell the value at postion.
-            int post; cin >> post;
-            cout << arr[post-1] + rangesum(segtree, 1, 0, n, 0, post-1)<< endl;
+            int v; cin >> v;
+            cout << arr[v-1] + rangesum(seg, 1, 0, n, 0, v-1) << endl;
         }
     }
-    return 0;
 }
+int main() {
+    FAST_FURIER;
+    int tt=1;
+    // cin >> tt;
+    while(tt--){
+        solve();
+    }
+}
+ 
