350c5feaf7fa5c5ac2971a309efcc561cfc5d02f
shivamsha-iitj <2019UCS0100@iitjammu.ac.in>
2021-07-02 23:16:38 +0530

    July 02

--- script.py
+++ script.py
@@ -5,12 +5,14 @@ from datetime import date
 #adding the changes
 process = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
 output = process.communicate()[0]
+print("Changes added...")
 time.sleep(2)   
 
 # committing the changes
 today = date.today().strftime("%B %d")
 process = subprocess.Popen(["git", "commit", "-m", today], stdout=subprocess.PIPE)
 output = process.communicate()[0]
+print("Committed the changes...")
 time.sleep(2)   
 
 # push the committed changes
