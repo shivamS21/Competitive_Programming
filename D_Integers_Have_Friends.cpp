803aeca20a8fab7fd8e7a9c96c3f39594f0264a7
shivamsha-iitj <2019UCS0100@iitjammu.ac.in>
2021-08-08 22:10:44 +0530

    August 08

--- D_Integers_Have_Friends.cpp
+++ D_Integers_Have_Friends.cpp
@@ -10,23 +10,14 @@ using namespace std;typedef long long ll;
 #define rrep(i,a,N)     for(ll i=a;i>N;i--)
 #define print(v)        for(ll ite=0;ite<v.size();ite++){cout<<v[ite]<<' ';}cout<<endl;
 #define M 1000000007
+const int N = 200000;
 bool comp(ll x,ll y){
     return x > y;
 }
- 
+ll segtree[4*N];
 /*...............code starts here................*/
 // C is first won in M
-const int N = 2e5+1;
-
-ll gcd(ll a, ll b)
-{
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
- 
-}
-void build(int segtree[], vector<int> diff, int vertex, int start, int end){
-    if(start > end) return;
+void build(ll segtree[], vector<ll> &diff, int vertex, ll start, ll end){
     if(start == end){
         segtree[vertex] = diff[start];
         return;
@@ -34,65 +25,61 @@ void build(int segtree[], vector<int> diff, int vertex, int start, int end){
     int mid = (start+end)/2;
     build(segtree, diff, 2*vertex, start, mid);
     build(segtree, diff, 2*vertex+1, mid+1, end);
-    segtree[vertex] = gcd(segtree[2*vertex], segtree[2*vertex+1]);
+    segtree[vertex] = __gcd(segtree[2*vertex], segtree[2*vertex+1]);
 }
- 
-int query(int segtree[], int vertex, int start, int end, int l , int r){
-    if(l > end or r < start) return 0;
+
+ll query(ll segtree[], int vertex, int start, int end, int l , int r){
     if(l > r) return 0;
     if(start == l and end == r) return segtree[vertex];
     int mid = (start+end)/2;
-    int a = query(segtree, 2*vertex, start, mid, l, min(r, mid));
-    int b = query(segtree, 2*vertex+1, mid+1, end, max(mid+1, l), r);
-    return gcd(a, b);
+    ll a = query(segtree, 2*vertex, start, mid, l, min(r, mid));
+    ll b = query(segtree, 2*vertex+1, mid+1, end, max(mid+1, l), r);
+    return __gcd(a, b);
 }
 void solve(){
     ll m,n,k;
     cin >> n;
-    vector<int> arr(n);
+    vector<ll> arr(n);
     rep(i,0,n){
         cin >> arr[i];
     }
-    vector<int> diff(n-1);
+    if(n==1){
+        cout << 1 << endl;
+        return;
+    }
+    vector<ll> diff(n-1);
     rep(i,0,n-1){
         diff[i] = abs(arr[i] - arr[i+1]);
     }
-    int segtree[4*N];
     build(segtree,diff, 1, 0, n-2);
-    int answer = 1;
-    int s = 0;
-    int e = 0;
-    int currGcd = diff[s];
-    while(e<n-1){
-        if(abs(diff[e]) == 1){
-            s = e +1;
-            e = s;
-            currGcd = diff[e];
+    ll ans = 1;
+    ll s = 0, e = 0;
+    ll currgcd = diff[0];
+    while(e < n-1){
+        if(diff[e] == 1){
+            s = ++e;
+            currgcd = diff[e]; 
             continue;
         }
-        answer = max(answer,e-s+2);
-        // dp we extend
-        if(e!=n-2){
-            if(abs(__gcd(currGcd,diff[e+1]))!=1){
-                currGcd = __gcd(currGcd,diff[e+1]);
-                e++;
+        ans = max(ans, e-s+2);
+        if(e != n-2){
+            ll temp = __gcd(currgcd, diff[e+1]);
+            if(temp != 1){
+                e += 1;
+                currgcd = temp;
                 continue;
-            }
+            } 
+        } 
+        ++s;
+        if(s > e){
+            e += 1;
+            currgcd = diff[s];
+        } else{
+            currgcd = query(segtree, 1, 0, n-2, s, e);
         }
-        // we shrink
-        s++;
-        if(s>e){
-            e++;
-            if(e == n-1) break;
-            currGcd = diff[s];
-        }else{
-            currGcd = query(segtree, 1, 0, n-2, s, e);
-        }
-
-
     }
     // cout << "check: " << query(segtree, 1, 0, n-2, 2, 3) << endl;
-    cout << answer<< endl;
+    cout << ans<< endl;
 }
 int main() {
     FAST_FURIER;
